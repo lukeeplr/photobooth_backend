9c14a82df7fa73a57160822d415976ab5062624b
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-09 04:23:59 -0300

    fix: organization switcher in wrong theme

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 export default function TopBar() {
   return (
@@ -24,6 +25,7 @@ export default function TopBar() {
         
           <OrganizationSwitcher
             appearance={{
+              baseTheme: dark,
               elements: {
                 organizationSwitcherTrigger: 'py-2 px-4'
               }
