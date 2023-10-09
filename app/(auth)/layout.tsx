532855a6c5f29ca03addc39d74b8bc9263c041b4
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-08 23:30:55 -0300

    feat: basic layout for auth routing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Inter } from "next/font/google";
+import '../globals.css';
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: 'Threads',
+    description: 'Threads feito com Next.js 13' 
+}
+
+export default function RootLayout({ 
+    children 
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider localization={ptBR}>
+            <html lang="pt-br">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
