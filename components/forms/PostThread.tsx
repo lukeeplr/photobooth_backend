7561986e46c182063e6c336b7140bcd92229e69e
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-12 03:29:24 -0300

    feat: post thread form

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Textarea } from '@/components/ui/textarea'
+
+import { ThreadValidation } from '@/lib/validations/thread'
+import { createThread } from '@/lib/actions/thread.actions'
+
+export default function PostThread({ userId }: { userId: string }) {
+
+  const pathname = usePathname()
+  const router = useRouter()
+
+    const form = useForm({
+        resolver: zodResolver(ThreadValidation),
+        defaultValues: {
+            thread: '',
+            author: userId
+        }
+    })
+
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+        await createThread({
+          author: userId,
+          thread: values.thread,
+          community: null,
+          path: pathname
+        })
+
+        router.push('/')     
+    }
+
+  return (
+    <Form {...form}>
+    <form 
+    onSubmit={form.handleSubmit(onSubmit)} 
+    className="flex flex-col justify-start gap-10 mt-10">
+    <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+              <FormItem>
+              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                <Textarea 
+                rows={15}
+                placeholder="O que está acontecendo?" {...field} 
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+        )}/>
+        <Button type="submit" className='bg-primary-500 hover:bg-primary-500/70 transition-all duration-500'>Publicar</Button>
+    </form>
+    </Form>
+  )
+}
