13684e086ca37e451e3ed1ef6721d88009b78ce4
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-13 02:00:45 -0300

    feat: fetch threads functionality with pagination

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -27,3 +27,37 @@ export async function createThread({author, thread, community, path}: createThre
 
     revalidatePath(path)
 }
+
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+    try {
+        connectToDB()
+
+        const skipAmout = ((pageNumber - 1) * pageSize)
+
+        const threadsQuery = Thread.find({ parentId: {$in: [null, undefined]}})
+        .sort({ createdAt: 'desc'})
+        .skip(skipAmout)
+        .limit(pageSize)
+        .populate({path: 'author', model: User})
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+            }
+            
+        })
+
+        const totalThreadsCount = await Thread.countDocuments({parentId: {$in: [null, undefined]}})
+        const threads = await threadsQuery.exec() 
+        const isNext = totalThreadsCount > skipAmout + threads.length
+
+        return { threads, isNext}
+
+
+    } catch (error: any) {
+        throw Error(`Falha ao buscar as threads: ${error.message}`)
+    }
+}
