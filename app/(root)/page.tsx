7d8df1b1a8052e334b687872c47e93ecefb674c1
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-13 02:00:10 -0300

    feat: add threads to the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,37 @@
 // import { UserButton } from "@clerk/nextjs";
- 
-export default function Home() {
+
+import ThreadCard from "@/components/cards/ThreadCard"
+import { fetchThreads } from "@/lib/actions/thread.actions"
+import { currentUser } from "@clerk/nextjs"
+
+export default async function Home() {
+  
+  const user = await currentUser()
+  const result = await fetchThreads(1, 20)
+
   return (
-    <h1 className="head-text">Home</h1>
+    <>
+    <h1 className="head-text">Início</h1>
+    <section className="mt-9 flex flex-col gap-10">
+      {result.threads.length === 0 
+      ? <p className="no-result">Nada pra ver agora...</p>     
+      : result.threads.map((thread) => {
+        return (
+          <ThreadCard 
+          key={thread._id}
+          id={thread._id}
+          currentUser={user?.id || ''}
+          parentId={thread.parentId}
+          content={thread.thread}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+          />
+        )        
+      }) 
+    }
+    </section>
+    </>
   )
 }
