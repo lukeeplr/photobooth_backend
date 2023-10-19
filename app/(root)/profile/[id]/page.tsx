68b84b6ab06aa5085a4e0cee1b13bf5734f88a16
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-19 18:45:49 -0300

    feat: profile page start

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,26 +1,71 @@
+import ProfileHeader from "@/components/shared/ProfileHeader"
 import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+import { profileTabs } from "@/constants"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Image from "next/image"
+import ThreadTab from "@/components/shared/ThreadTab"
+
 
 export default async function Page({ params }: { params: {id: string}}) {
 
-    const user = currentUser()
+    const user = await currentUser()
 
     if(!user) return
 
     const userInfo = await fetchUser(params.id)
 
+    if (!userInfo.onboarded) redirect('/onboarding')
+
     return (
         <section>
-            <h1 className="head-text">Editar perfil</h1>
-            <div className="mt-10">
-            <Image 
-            src={userInfo.image} 
-            alt='foto de perfil' 
-            width={100} 
-            height={100}
-            className="rounded-full object-cover"
+            <ProfileHeader 
+                profileId={userInfo.id}
+                loggedId={user.id}
+                name={userInfo.name}
+                username={userInfo.username}
+                image={userInfo.image}
+                bio={userInfo.bio}
             />
+
+            <div className="mt-9">
+                <Tabs defaultValue="threads" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger 
+                            key={tab.label}
+                            value={tab.value}
+                            className="tab cursor-pointer "
+                            >
+                                <Image 
+                                src={tab.icon}
+                                alt={tab.label}
+                                width={24}
+                                height={24}
+                                className="object-contain"
+                                />
+                                <p className="max-sm:hidden">{tab.label}</p>
+                                {tab.label === 'Threads' && (
+                                    <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">{userInfo?.threads?.length}</p>
+                                    )}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {profileTabs.map((tabs) => (
+                        <TabsContent 
+                        key={tabs.label} 
+                        value={tabs.value}
+                        className="w-full text-light-1"
+                        >
+                            <ThreadTab 
+                            currentUserId={user.id}
+                            accountId={userInfo.id}
+                            accountType='User'
+                            />
+                        </TabsContent>
+                    ))}
+                </Tabs>
             </div>
         </section>
     )
