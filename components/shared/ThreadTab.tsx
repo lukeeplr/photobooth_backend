825d98a4619492a3471b7faf3d5f0b9f5a74582f
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-19 18:46:15 -0300

    feat: profile threads tab component

---
+++ components/shared/ThreadTab.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type ThreadTypeProps = {
+    currentUserId: string
+    accountId: string
+    accountType: string
+}
+
+export default function ThreadTab({currentUserId, accountId, accountType}: ThreadTypeProps) {
+  return (
+
+    //TODO: Fetch threads do usuario
+    <section>
+        ThreadTab
+    </section>
+  )
+}
