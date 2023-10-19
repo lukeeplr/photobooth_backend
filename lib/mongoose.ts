b7837ea74e40a2e58fe3ca20f6a25d3ab7a9d709
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-18 23:01:05 -0300

    refactor: remove console logs

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,18 +6,18 @@ export const connectToDB = async () => {
     mongoose.set('strictQuery', true)
     
     if (!process.env.MONGODB_URL) {
-        console.log('MONGODB_URL não encontrada')
+        //console.log('MONGODB_URL não encontrada')
     }
     
     if (isConnected) {
-        return console.log('Já está conectado ao MongoDB')
+        // return console.log('Já está conectado ao MongoDB')
     }
         
     try {
 
         await mongoose.connect(process.env.MONGODB_URL as string)
         isConnected = true
-        console.log('MongoDB conectado')
+        // console.log('MongoDB conectado')
 
     } catch (error) {
         console.log(error)
