be87089893890ecf8d06d081ad079d0dbe30dc5d
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-20 23:55:16 -0300

    feat: user thread tab

--- components/shared/ThreadTab.tsx
+++ components/shared/ThreadTab.tsx
@@ -1,4 +1,7 @@
+import { fetchUserPosts } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import ThreadCard from '../cards/ThreadCard'
 
 type ThreadTypeProps = {
     currentUserId: string
@@ -6,12 +9,32 @@ type ThreadTypeProps = {
     accountType: string
 }
 
-export default function ThreadTab({currentUserId, accountId, accountType}: ThreadTypeProps) {
+export default async function ThreadTab({currentUserId, accountId, accountType}: ThreadTypeProps) {
+  
+  let result = await fetchUserPosts(accountId)
+
+  if(!result) redirect('/')
+  
   return (
 
-    //TODO: Fetch threads do usuario
-    <section>
-        ThreadTab
+    <section className='mt-9 flex flex-col gap-10'>
+        {result.threads.map((thread: any) => (
+          <ThreadCard 
+          key={thread._id}
+          id={thread._id}
+          currentUser={currentUserId}
+          parentId={thread.parentId}
+          content={thread.thread}
+          author={
+            accountType === 'User'
+              ? { name: result.name, username: result.username, image: result.image, id: result.id}
+              : { name: thread.author.name, username: thread.author.username, image: thread.author.image, id: thread.author.id}
+        }
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+          />
+        ))}
     </section>
   )
 }
