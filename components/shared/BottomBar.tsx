d3953d5d8fb0f91cd44adf585a1a771f6c016dfb
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-21 03:10:33 -0300

    fix: bottom menu profile button not working properly

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -6,11 +6,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { sidebarLinks } from '@/constants'
+import { useAuth } from '@clerk/nextjs'
 
 
 export default function BottomBar() {
   
   const pathname = usePathname()
+  const { userId } = useAuth()
 
   return (
     <section className='bottombar'>
@@ -20,6 +22,9 @@ export default function BottomBar() {
             (((pathname.includes(link.route)) && link.route.length > 1) 
             || pathname === link.route)
           )
+
+          if (link.route === '/profile') link.route = `/profile/${userId}`
+
           return (
           <Link href={link.route} key={index} className={`bottombar_link ${isActive ? 'bg-primary-500' : 'hover:bg-primary-500/20'}`}>
             <Image src={link.imgURL} alt={link.label} width={24} height={24}/>
