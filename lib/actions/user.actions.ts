b0233d7126ed40a38646047e39a60702b07170ef
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-23 01:10:48 -0300

    feat: fetch users and activity function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model"
+import { FilterQuery, SortOrder } from "mongoose"
 
 type updateUserParams = {
     userId: string,
@@ -13,6 +14,16 @@ type updateUserParams = {
     image: string,
     path: string
 }
+
+
+type fetchUsersParams = {
+    userId: string,
+    searchTerm?: string,
+    pageNumber?: number,
+    pageSize?: number,
+    sortBy?: SortOrder
+}
+
 
 export async function updateUser({
     userId,
@@ -88,3 +99,79 @@ export async function fetchUserPosts(userId: string) {
         throw Error(`Falha ao encontrar os posts do usuário: ${error.message}`)
     }
 }
+
+
+export async function fetchUsers({
+    userId,
+    searchTerm = '',
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = 'desc' 
+
+}: fetchUsersParams) {
+    
+    try {
+        connectToDB()
+
+        const skipAmout = ((pageNumber - 1) * pageSize)
+        
+        const regex = new RegExp(searchTerm, 'i')
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+
+        if(searchTerm.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex}},
+                { name: { $regex: regex}}
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy}
+
+        const usersQuery = User.find(query) 
+            .sort(sortOptions)
+            .skip(skipAmout)
+            .limit(pageSize)
+    
+
+        const totalUsersCount = await User.countDocuments(query)
+
+        const users = await usersQuery.exec()
+
+        const isNext = totalUsersCount > skipAmout + users.length
+
+        return { users, isNext}
+
+    } catch (error: any) {
+        throw new Error(`Falha ao buscar usuários: ${error.message}`)
+    } 
+}
+
+
+export async function fetchActivity(userId: string) {
+
+    try {
+
+        connectToDB()
+        const userThreads = await Thread.find({ author: userId })
+
+        const childThreadsId = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        }, [])
+
+        const replies = await Thread.find({
+             _id: { $in: childThreadsId },
+            author: { $ne: userId }}
+        ).populate({ 
+            path: 'author', 
+            model: User,
+            select: '_id name image'})
+
+        return replies
+
+    } catch (error: any) {
+        throw new Error(`Falha ao buscar notificações: ${error.message}`)
+    }
+} 
