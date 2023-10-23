7781622c81c8fa13684230d0921aa2f13fd212be
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-23 01:06:35 -0300

    feat: notifications page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { fetchActivity, fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { redirect } from "next/navigation"
+
 
 export default async function Page() {
+
+    const user = await currentUser()
+
+    if(!user) return
+
+    const userInfo = await fetchUser(user.id)
+
+    if (!userInfo.onboarded) redirect('/onboarding')
+
+    const results = await fetchActivity(userInfo._id)
+
   return (
     <section>
         <h1 className="head-text mb-10">Atividade</h1>
+
+        <section className="mt-10 flex flex-col gap-5">
+          {results.length > 0
+          ? (
+            <>
+            {results.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card gap-5">
+                  <Image
+                  src={activity.author.image}
+                  alt={`foto de perfil de @${activity.author.username}`} 
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                  />
+                  <p className="!text-base-regular text-light-2/80 ">
+                    <span className="text-base-semibold text-light-1">
+                      {activity.author.name}</span> {" "}respondeu sua Thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+            </>
+          )
+          : (
+            <p className="text-left no-result">Nenhuma atividade encontrada</p>
+          )
+        }
+        </section>
     </section>
   )
 }
