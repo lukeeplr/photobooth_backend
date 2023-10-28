d01e20f689b7d95fbbe454fc9c31272633d036dc
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-10-28 19:08:15 -0300

    fix: redirect onboarded users and updates user data

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,22 +2,23 @@ import { currentUser } from '@clerk/nextjs'
 
 import AccountProfile from '@/components/forms/AccountProfile'
 import { fetchUser } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
 async function Page() {
 
     const user = await currentUser()
-
     if (!user) return
 
     const userInfo = await fetchUser(user.id)
+    if(userInfo?.onboarded) redirect('/onboarding')
 
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user?.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user?.imageUrl
+        username: userInfo? userInfo?.username : user?.username,
+        name: userInfo? userInfo?.name : user?.firstName || "",
+        bio: userInfo? userInfo?.bio : "",
+        image: userInfo? userInfo?.image : user?.imageUrl
     }
 
     return (
